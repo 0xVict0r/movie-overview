2e34461cd22f27132544ca32c4b7c7a9b807bb04
0xVict0r <74269639+0xVict0r@users.noreply.github.com>
2022-08-01 19:29:56 +0200

    changed description

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Movie Overview & Rating Aggregator
-A tool that aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.
+When looking at potential movies to watch, I always found it boring to look at multiple websites to see if it was even worth my time, or if the website I was looking at was biased in any way. Hence why I created this movie lookup tool. It aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.
 
 You can find the app at the [link](https://movie-app.morizon.dev/)
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 
 st.title("Movie Overview & Rating Aggregator")
 
-st.write("A tool that aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.")
+st.write("When looking at potential movies to watch, I always found it boring to look at multiple websites to see if it was even worth my time, or if the website I was looking at was biased in any way. Hence why I created this movie lookup tool. It aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.")
 
 st.markdown(
     "<style>.css-15zrgzn {display: none}</style>", unsafe_allow_html=True)
