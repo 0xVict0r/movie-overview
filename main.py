a156ef15e0e616f1cbdadf325be85ee704933509
0xVict0r <74269639+0xVict0r@users.noreply.github.com>
2022-08-03 08:15:47 +0200

    refactored

--- main.py
+++ main.py
@@ -4,48 +4,55 @@ from movie_class import Movie
 import functions
 import numpy as np
 
-api_key = st.secrets["tmdb_api"]
 
-st.set_page_config(layout="wide")
+def main():
+    api_key = st.secrets["tmdb_api"]
 
-st.title("Movie Overview & Rating Aggregator")
+    st.set_page_config(layout="wide")
 
-st.write("When looking at potential movies to watch, I always found it boring to look at multiple websites to see if it was even worth my time, or if the website I was looking at was biased in any way. Hence why I created this movie lookup tool. It aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.")
+    st.title("Movie Overview & Rating Aggregator")
 
-st.markdown(
-    "<style>.css-15zrgzn {display: none}</style>", unsafe_allow_html=True)
-st.markdown(
-    """<style> div.stButton > button:first-child { width: 100% ; } </style>""", unsafe_allow_html=True)
+    st.write("When looking at potential movies to watch, I always found it boring to look at multiple websites to see if it was even worth my time, or if the website I was looking at was biased in any way. Hence why I created this movie lookup tool. It aggregates movie data from multiple sources (TMDB, IMDB, Rotten Tomatoes, Allocine) to give a nice overview. It also gives an aggregated movie rating from the before mentionned sources. Simply enter the name of the movie (no TV) and the program will find the relevant information for you.")
 
-with st.sidebar:
-    with st.form("Main Form"):
+    st.markdown(
+        "<style>.css-15zrgzn {display: none}</style>", unsafe_allow_html=True)
+    st.markdown(
+        """<style> div.stButton > button:first-child { width: 100% ; } </style>""", unsafe_allow_html=True)
 
-        search = st.text_input("Enter the title of the Movie: ")
+    with st.sidebar:
+        with st.form("Main Form"):
 
-        submit_bttn = st.form_submit_button("Search")
+            search = st.text_input("Enter the title of the Movie: ")
 
-if submit_bttn:
-    query = search.replace(" ", "%20")
+            submit_bttn = st.form_submit_button("Search")
 
-    movie_id = httpx.get(
-        f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={query}&page=1&include_adult=false").json()["results"][0]["id"]
+    if submit_bttn:
+        query = search.replace(" ", "%20")
 
-    movie = Movie(movie_id, api_key)
+        movie_id = httpx.get(
+            f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={query}&page=1&include_adult=false").json()["results"][0]["id"]
 
-    functions.plot_general_info(movie)
+        movie = Movie(movie_id, api_key)
 
-    imdb_rating = functions.safe_execute(
-        None, (AttributeError, TypeError), movie.get_imdb_rating)
-    rt_rating = functions.safe_execute(
-        None, (AttributeError, TypeError, IndexError, ValueError), movie.get_rt_ratings)
-    allocine_rating = functions.safe_execute(
-        None, (AttributeError, TypeError, IndexError, ValueError), movie.get_allocine_ratings)
+        functions.plot_general_info(movie)
 
-    ratings_list = [imdb_rating, movie.tmdb_rating, rt_rating, allocine_rating]
+        imdb_rating = functions.safe_execute(
+            None, (AttributeError, TypeError), movie.get_imdb_rating)
+        rt_rating = functions.safe_execute(
+            None, (AttributeError, TypeError, IndexError, ValueError), movie.get_rt_ratings)
+        allocine_rating = functions.safe_execute(
+            None, (AttributeError, TypeError, IndexError, ValueError), movie.get_allocine_ratings)
 
-    rating = np.round(np.mean(
-        np.array([rating for rating in ratings_list if type(rating) != type(None)]))*10, 1)
+        ratings_list = [imdb_rating, movie.tmdb_rating,
+                        rt_rating, allocine_rating]
 
-    functions.plot_cast(movie_id)
+        rating = np.round(np.mean(
+            np.array([rating for rating in ratings_list if type(rating) != type(None)]))*10, 1)
 
-    functions.plot_gauge(rating)
+        functions.plot_cast(movie_id)
+
+        functions.plot_gauge(rating)
+
+
+if __name__ == '__main__':
+    main()
